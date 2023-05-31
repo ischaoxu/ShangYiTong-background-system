9cab4452d624c542acf0ebb78a7748f7dbd2fe30
liuzhaoxu <1244453574@qq.com>
2023-05-31 18:41:20 +0800

    新增医院设置

---
+++ service/service-hosp/src/main/java/com/atguigu/HospApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.atguigu"})
+public class HospApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HospApplication.class, args);
+    }
+}
