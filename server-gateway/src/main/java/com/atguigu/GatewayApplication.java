4e85df9d130f5fd926f744429ef285436984e36f
liuzhaoxu <1244453574@qq.com>
2023-06-04 18:19:32 +0800

    网关引入

--- server-gateway/src/main/java/com/atguigu/GatewayApplication.java
+++ server-gateway/src/main/java/com/atguigu/GatewayApplication.java
@@ -1,15 +1,13 @@
-package com.atguigu.common.system;
+package com.atguigu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
+
 @EnableDiscoveryClient
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.atguigu"})
-public class ServiceSystemApplication {
-
+public class GatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ServiceSystemApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 }
